8a5844b957fbc758a13bc81cd9572abf1795f5ba
François Guérin <francoisguerin1@hotmail.fr>
2024-11-01 21:25:28 +0100

    Make `Stack.pop` return an `Option<u256>`

--- src/stack.rs
+++ src/stack.rs
@@ -11,16 +11,16 @@ impl Stack {
         Self { size: s.unwrap_or(1024), top: -1, arr: Vec::<u256>::new() }
     }
 
-    fn pop(&mut self) -> Result<u256, &str> {
+    fn pop(&mut self) -> Option<u256> {
         match self.arr.pop() {
-            Option::None => Err("Stack is empty"),
-            Option::Some(elt) => { self.top -= 1; Ok(elt) },
+            Option::None => None,
+            Option::Some(elt) => { self.top -= 1; Some(elt) },
         }
     }
 
     fn push(&mut self, value: u256) -> Result<i32, &str> {
         match self.top + 1 == self.size.into() {
-            true => Err("Stack is full"),
+            true => Err("Stack overflow"),
             false => { self.top += 1; self.arr.push(value); Ok(self.top) },
         }
     }
@@ -35,14 +35,14 @@ mod tests {
     fn fail_to_pop_from_an_empty_stack() {
         let mut stack = Stack::new(Option::None);
 
-        assert_eq!(stack.pop(), Err("Stack is empty"));
+        assert_eq!(stack.pop(), None);
     }
 
     #[test]
     fn pops_from_the_stack() {
         let mut stack = Stack { size: 1, top: 0, arr: vec![uint!("7")] };
 
-        assert_eq!(stack.pop(), Ok(uint!("7")));
+        assert_eq!(stack.pop(), Some(uint!("7")));
         assert_eq!(stack.size, 1);
         assert_eq!(stack.top, -1);
         assert_eq!(stack.arr.len(), 0);
@@ -52,7 +52,7 @@ mod tests {
     fn fail_to_push_to_an_already_full_stack() {
         let mut stack = Stack::new(Option::Some(0));
 
-        assert_eq!(stack.push(uint!("7")), Err("Stack is full"));
+        assert_eq!(stack.push(uint!("7")), Err("Stack overflow"));
     }
 
     #[test]
