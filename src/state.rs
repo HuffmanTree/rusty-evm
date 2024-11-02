cfdfe0702964fe289efd5204c568e3e617dd099a
François Guérin <francoisguerin1@hotmail.fr>
2024-11-02 13:31:04 +0100

    Implement `SDIV`

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,38 @@
 use ethnum::u256;
 use crate::stack::Stack;
 
+trait IsNeg {
+    fn is_neg(&self) -> bool;
+}
+
+trait Abs {
+    fn abs(&self) -> Self;
+}
+
+trait WrappingSignedDiv {
+    fn wrapping_signed_div(&self, rhs: Self) -> Self;
+}
+
+impl IsNeg for u256 {
+    fn is_neg(&self) -> bool {
+        (self & u256::from_str_hex("0x8000000000000000000000000000000000000000000000000000000000000000").unwrap()) != 0
+    }
+}
+
+impl Abs for u256 {
+    fn abs(&self) -> Self {
+        if self.is_neg() { self.wrapping_neg() } else { self.clone() }
+    }
+}
+
+impl WrappingSignedDiv for u256 {
+    fn wrapping_signed_div(&self, rhs: Self) -> Self {
+        let negate = self.is_neg() ^ rhs.is_neg();
+        let res = self.abs().wrapping_div(rhs.abs());
+        if negate { res.wrapping_neg() } else { res }
+    }
+}
+
 struct State {
     stack: Stack,
     stop_flag: bool,
@@ -68,12 +100,51 @@ impl State {
             Err(s) => Err(s),
         }
     }
+
+    fn sdiv(&mut self) -> Result<TransitionOutput, &str> {
+        let (a, b) = match (self.stack.pop(), self.stack.pop()) {
+            (Some(x), Some(y)) => (x, y),
+            _ => return Err("Stack is empty"),
+        };
+        match self.stack.push(if b == 0 { u256::from(0_u8) } else { a.wrapping_signed_div(b) }) {
+            Ok(_) => Ok(TransitionOutput { cost: 5, jump: 1 }),
+            Err(s) => Err(s),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use ethnum::uint;
+
+    #[test]
+    fn u256_is_neg() {
+        assert!(!uint!("6").is_neg());
+        assert!(!uint!("10").is_neg());
+
+        assert!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE").is_neg());
+        assert!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").is_neg());
+    }
+
+    #[test]
+    fn u256_abs() {
+        assert_eq!(uint!("6").abs(), uint!("6"));
+        assert_eq!(uint!("10").abs(), uint!("10"));
+
+        assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE").abs(), uint!("2"));
+        assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").abs(), uint!("1"));
+    }
+
+    #[test]
+    #[should_panic(expected = "attempt to divide by zero")]
+    fn u256_wrapping_signed_div() {
+        assert_eq!(uint!("4").wrapping_signed_div(uint!("2")), uint!("2"));
+        assert_eq!(uint!("4").wrapping_signed_div(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")), uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE"));
+        assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC").wrapping_signed_div(uint!("2")), uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE"));
+        assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC").wrapping_signed_div(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")), uint!("2"));
+        uint!("4").wrapping_signed_div(uint!("0"));
+    }
 
     #[test]
     fn set_the_stop_flag_to_true() {
@@ -201,5 +272,51 @@ mod tests {
         let mut state = State::new();
 
         assert_eq!(state.div(), Err("Stack is empty"));
+    }
+
+    #[test]
+    fn sign_divides_the_two_numbers_on_top_of_the_stack() {
+        let mut state = State::new();
+        state.stack.push(uint!("5")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+        state.stack.push(uint!("10")).unwrap();
+
+        assert_eq!(state.sdiv(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("1")));
+        assert_eq!(state.stack.pop(), Some(uint!("5")));
+        assert_eq!(state.stack.pop(), None);
+    }
+
+    #[test]
+    fn sign_divides_with_negations() {
+        let mut state = State::new();
+        state.stack.push(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")).unwrap();
+        state.stack.push(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")).unwrap();
+
+        assert_eq!(state.sdiv(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("2")));
+
+        state.stack.push(uint!("1")).unwrap();
+        state.stack.push(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")).unwrap();
+
+        assert_eq!(state.sdiv(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")));
+    }
+
+    #[test]
+    fn sign_dividing_by_zero_returns_zero_by_convention() {
+        let mut state = State::new();
+        state.stack.push(uint!("0")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+
+        assert_eq!(state.sdiv(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0")));
+    }
+
+    #[test]
+    fn fails_to_sign_divide_if_not_enough_items() {
+        let mut state = State::new();
+
+        assert_eq!(state.sdiv(), Err("Stack is empty"));
     }
 }
