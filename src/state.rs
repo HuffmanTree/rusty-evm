ef0f9056b5480ebc9c36a19bab7a187eb696d24a
François Guérin <francoisguerin1@hotmail.fr>
2024-11-02 14:57:35 +0100

    Implement `MOD` and `SMOD`

--- src/state.rs
+++ src/state.rs
@@ -13,6 +13,10 @@ trait WrappingSignedDiv {
     fn wrapping_signed_div(&self, rhs: Self) -> Self;
 }
 
+trait WrappingSignedRem {
+    fn wrapping_signed_rem(&self, rhs: Self) -> Self;
+}
+
 impl IsNeg for u256 {
     fn is_neg(&self) -> bool {
         (self & u256::from_str_hex("0x8000000000000000000000000000000000000000000000000000000000000000").unwrap()) != 0
@@ -29,6 +33,14 @@ impl WrappingSignedDiv for u256 {
     fn wrapping_signed_div(&self, rhs: Self) -> Self {
         let negate = self.is_neg() ^ rhs.is_neg();
         let res = self.abs().wrapping_div(rhs.abs());
+        if negate { res.wrapping_neg() } else { res }
+    }
+}
+
+impl WrappingSignedRem for u256 {
+    fn wrapping_signed_rem(&self, rhs: Self) -> Self {
+        let negate = self.is_neg();
+        let res = self.abs().wrapping_rem(rhs.abs());
         if negate { res.wrapping_neg() } else { res }
     }
 }
@@ -111,6 +123,36 @@ impl State {
             Err(s) => Err(s),
         }
     }
+
+    fn r#mod(&mut self) -> Result<TransitionOutput, &str> {
+        let (a, b) = match (self.stack.pop(), self.stack.pop()) {
+            (Some(x), Some(y)) => (x, y),
+            _ => return Err("Stack is empty"),
+        };
+        match self.stack.push(if b == 0 { u256::from(0_u8) } else { a.wrapping_rem(b) }) {
+            Ok(_) => Ok(TransitionOutput { cost: 5, jump: 1 }),
+            Err(s) => Err(s),
+        }
+    }
+
+    fn smod(&mut self) -> Result<TransitionOutput, &str> {
+        let (a, b) = match (self.stack.pop(), self.stack.pop()) {
+            (Some(x), Some(y)) => (x, y),
+            _ => return Err("Stack is empty"),
+        };
+        match self.stack.push(if b == 0 { u256::from(0_u8) } else { a.wrapping_signed_rem(b) }) {
+            Ok(_) => Ok(TransitionOutput { cost: 5, jump: 1 }),
+            Err(s) => Err(s),
+        }
+    }
+
+    fn addmod(&mut self) -> Result<TransitionOutput, &str> {
+        todo!();
+    }
+
+    fn mulmod(&mut self) -> Result<TransitionOutput, &str> {
+        todo!();
+    }
 }
 
 #[cfg(test)]
@@ -144,6 +186,16 @@ mod tests {
         assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC").wrapping_signed_div(uint!("2")), uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE"));
         assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC").wrapping_signed_div(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")), uint!("2"));
         uint!("4").wrapping_signed_div(uint!("0"));
+    }
+
+    #[test]
+    #[should_panic(expected = "attempt to calculate the remainder with a divisor of zero")]
+    fn u256_wrapping_signed_rem() {
+        assert_eq!(uint!("5").wrapping_signed_rem(uint!("2")), uint!("1"));
+        assert_eq!(uint!("5").wrapping_signed_rem(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")), uint!("1"));
+        assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFB").wrapping_signed_rem(uint!("2")), uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
+        assert_eq!(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFB").wrapping_signed_rem(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")), uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
+        uint!("5").wrapping_signed_rem(uint!("0"));
     }
 
     #[test]
@@ -315,6 +367,94 @@ mod tests {
 
     #[test]
     fn fails_to_sign_divide_if_not_enough_items() {
+        let mut state = State::new();
+
+        assert_eq!(state.sdiv(), Err("Stack is empty"));
+    }
+
+    #[test]
+    fn takes_the_reminder_of_the_two_numbers_on_top_of_the_stack() {
+        let mut state = State::new();
+        state.stack.push(uint!("5")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+        state.stack.push(uint!("10")).unwrap();
+
+        assert_eq!(state.r#mod(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("4")));
+        assert_eq!(state.stack.pop(), Some(uint!("5")));
+        assert_eq!(state.stack.pop(), None);
+    }
+
+    #[test]
+    fn taking_the_reminder_by_zero_returns_zero_by_convention() {
+        let mut state = State::new();
+        state.stack.push(uint!("0")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+
+        assert_eq!(state.r#mod(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0")));
+    }
+
+    #[test]
+    fn fails_to_take_the_reminder_if_not_enough_items() {
+        let mut state = State::new();
+
+        assert_eq!(state.r#mod(), Err("Stack is empty"));
+    }
+
+    #[test]
+    fn sign_rems_the_two_numbers_on_top_of_the_stack() {
+        let mut state = State::new();
+        state.stack.push(uint!("5")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+        state.stack.push(uint!("10")).unwrap();
+
+        assert_eq!(state.smod(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("4")));
+        assert_eq!(state.stack.pop(), Some(uint!("5")));
+        assert_eq!(state.stack.pop(), None);
+    }
+
+    #[test]
+    fn sign_rems_with_negations() {
+        let mut state = State::new();
+        state.stack.push(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFD")).unwrap();
+        state.stack.push(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF8")).unwrap();
+
+        assert_eq!(state.smod(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")));
+
+        state.stack.push(uint!("2")).unwrap();
+        state.stack.push(uint!("3")).unwrap();
+
+        assert_eq!(state.smod(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("1")));
+
+        state.stack.push(uint!("2")).unwrap();
+        state.stack.push(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFD")).unwrap();
+
+        assert_eq!(state.smod(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")));
+
+        state.stack.push(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE")).unwrap();
+        state.stack.push(uint!("3")).unwrap();
+
+        assert_eq!(state.smod(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("1")));
+    }
+
+    #[test]
+    fn sign_reming_by_zero_returns_zero_by_convention() {
+        let mut state = State::new();
+        state.stack.push(uint!("0")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+
+        assert_eq!(state.sdiv(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0")));
+    }
+
+    #[test]
+    fn fails_to_sign_rem_if_not_enough_items() {
         let mut state = State::new();
 
         assert_eq!(state.sdiv(), Err("Stack is empty"));
