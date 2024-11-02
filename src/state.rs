f92a748f8ee20be32883caf9289051d47a03d719
François Guérin <francoisguerin1@hotmail.fr>
2024-11-02 11:11:29 +0100

    Implement `SUB` and `DIV`

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,4 @@
+use ethnum::u256;
 use crate::stack::Stack;
 
 struct State {
@@ -41,6 +42,28 @@ impl State {
             _ => return Err("Stack is empty"),
         };
         match self.stack.push(a.wrapping_mul(b)) {
+            Ok(_) => Ok(TransitionOutput { cost: 5, jump: 1 }),
+            Err(s) => Err(s),
+        }
+    }
+
+    fn sub(&mut self) -> Result<TransitionOutput, &str> {
+        let (a, b) = match (self.stack.pop(), self.stack.pop()) {
+            (Some(x), Some(y)) => (x, y),
+            _ => return Err("Stack is empty"),
+        };
+        match self.stack.push(a.wrapping_sub(b)) {
+            Ok(_) => Ok(TransitionOutput { cost: 3, jump: 1 }),
+            Err(s) => Err(s),
+        }
+    }
+
+    fn div(&mut self) -> Result<TransitionOutput, &str> {
+        let (a, b) = match (self.stack.pop(), self.stack.pop()) {
+            (Some(x), Some(y)) => (x, y),
+            _ => return Err("Stack is empty"),
+        };
+        match self.stack.push(if b == 0 { u256::from(0_u8) } else { a.wrapping_div(b) }) {
             Ok(_) => Ok(TransitionOutput { cost: 5, jump: 1 }),
             Err(s) => Err(s),
         }
@@ -118,5 +141,65 @@ mod tests {
         let mut state = State::new();
 
         assert_eq!(state.mul(), Err("Stack is empty"));
+    }
+
+    #[test]
+    fn subtracts_the_two_numbers_on_top_of_the_stack() {
+        let mut state = State::new();
+        state.stack.push(uint!("5")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+        state.stack.push(uint!("10")).unwrap();
+
+        assert_eq!(state.sub(), Ok(TransitionOutput { cost: 3, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("4")));
+        assert_eq!(state.stack.pop(), Some(uint!("5")));
+        assert_eq!(state.stack.pop(), None);
+    }
+
+    #[test]
+    fn subtracts_with_an_overflow() {
+        let mut state = State::new();
+        state.stack.push(uint!("1")).unwrap();
+        state.stack.push(uint!("0")).unwrap();
+
+        assert_eq!(state.sub(), Ok(TransitionOutput { cost: 3, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")));
+    }
+
+    #[test]
+    fn fails_to_subtract_if_not_enough_items() {
+        let mut state = State::new();
+
+        assert_eq!(state.sub(), Err("Stack is empty"));
+    }
+
+    #[test]
+    fn divides_the_two_numbers_on_top_of_the_stack() {
+        let mut state = State::new();
+        state.stack.push(uint!("5")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+        state.stack.push(uint!("10")).unwrap();
+
+        assert_eq!(state.div(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("1")));
+        assert_eq!(state.stack.pop(), Some(uint!("5")));
+        assert_eq!(state.stack.pop(), None);
+    }
+
+    #[test]
+    fn dividing_by_zero_returns_zero_by_convention() {
+        let mut state = State::new();
+        state.stack.push(uint!("0")).unwrap();
+        state.stack.push(uint!("6")).unwrap();
+
+        assert_eq!(state.div(), Ok(TransitionOutput { cost: 5, jump: 1 }));
+        assert_eq!(state.stack.pop(), Some(uint!("0")));
+    }
+
+    #[test]
+    fn fails_to_divide_if_not_enough_items() {
+        let mut state = State::new();
+
+        assert_eq!(state.div(), Err("Stack is empty"));
     }
 }
