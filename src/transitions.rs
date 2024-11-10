5fc7c1b7c43a50d6d5c68e5ec8f7c46ad1eb0722
François Guérin <francoisguerin1@hotmail.fr>
2024-11-10 10:46:20 +0100

    Remove unnecessary parameter type annotations in transition functions

--- src/transitions.rs
+++ src/transitions.rs
@@ -19,17 +19,17 @@ pub struct TransitionOutput {
     pub jump: usize,
 }
 
-pub static ADD: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [a.wrapping_add(b)], jump: 1 };
-pub static MUL: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 5, result: [a.wrapping_mul(b)], jump: 1 };
-pub static SUB: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [a.wrapping_sub(b)], jump: 1 };
-pub static DIV: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_div(b) }], jump: 1 };
-pub static SDIV: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_signed_div(b) }], jump: 1 };
-pub static MOD: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_rem(b) }], jump: 1 };
-pub static SMOD: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_signed_rem(b) }], jump: 1 };
-pub static ADDMOD: TransitionFunction<3, 1> = |[a, b, n]: [u256; 3]| TransitionFunctionOutput { cost: 8, result: [if n == 0 { U256::ZERO } else { a.wrapping_rem(n).wrapping_add(b.wrapping_rem(n)).wrapping_rem(n) }], jump: 1 };
-pub static MULMOD: TransitionFunction<3, 1> = |[a, b, n]: [u256; 3]| TransitionFunctionOutput { cost: 8, result: [if n == 0 { U256::ZERO } else { a.wrapping_rem(n).wrapping_mul(b.wrapping_rem(n)).wrapping_rem(n) }], jump: 1 };
-pub static EXP: TransitionFunction<2, 1> = |[a, e]: [u256; 2]| TransitionFunctionOutput { cost: 10 + 50 * e.needed_size_in_bytes(), result: [a.wrapping_big_pow(e)], jump: 1 };
-pub static SIGNEXTEND: TransitionFunction<2, 1> = |[b, x]: [u256; 2]| {
+pub static ADD: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [a.wrapping_add(b)], jump: 1 };
+pub static MUL: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 5, result: [a.wrapping_mul(b)], jump: 1 };
+pub static SUB: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [a.wrapping_sub(b)], jump: 1 };
+pub static DIV: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_div(b) }], jump: 1 };
+pub static SDIV: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_signed_div(b) }], jump: 1 };
+pub static MOD: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_rem(b) }], jump: 1 };
+pub static SMOD: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 5, result: [if b == 0 { U256::ZERO } else { a.wrapping_signed_rem(b) }], jump: 1 };
+pub static ADDMOD: TransitionFunction<3, 1> = |[a, b, n]| TransitionFunctionOutput { cost: 8, result: [if n == 0 { U256::ZERO } else { a.wrapping_rem(n).wrapping_add(b.wrapping_rem(n)).wrapping_rem(n) }], jump: 1 };
+pub static MULMOD: TransitionFunction<3, 1> = |[a, b, n]| TransitionFunctionOutput { cost: 8, result: [if n == 0 { U256::ZERO } else { a.wrapping_rem(n).wrapping_mul(b.wrapping_rem(n)).wrapping_rem(n) }], jump: 1 };
+pub static EXP: TransitionFunction<2, 1> = |[a, e]| TransitionFunctionOutput { cost: 10 + 50 * e.needed_size_in_bytes(), result: [a.wrapping_big_pow(e)], jump: 1 };
+pub static SIGNEXTEND: TransitionFunction<2, 1> = |[b, x]| {
     let b: u32 = min(b, u256::from(30_u32)).try_into().unwrap();
     let mask = U256::ONE.wrapping_shl((b + 1).wrapping_shl(3));
     let sign_mask = mask.wrapping_shr(1);
@@ -37,34 +37,34 @@ pub static SIGNEXTEND: TransitionFunction<2, 1> = |[b, x]: [u256; 2]| {
     let value = x & size_mask;
     TransitionFunctionOutput { cost: 5, result: [if (value & sign_mask) != 0 { !size_mask | value } else { value }], jump: 1 }
 };
-pub static LT: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [if a < b { U256::ONE } else { U256::ZERO }], jump: 1 };
-pub static GT: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [if a > b { U256::ONE } else { U256::ZERO }], jump: 1 };
-pub static SLT: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [match (a.is_neg(), b.is_neg()) {
+pub static LT: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [if a < b { U256::ONE } else { U256::ZERO }], jump: 1 };
+pub static GT: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [if a > b { U256::ONE } else { U256::ZERO }], jump: 1 };
+pub static SLT: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [match (a.is_neg(), b.is_neg()) {
     (true, false) => { U256::ONE },
     (false, true) => { U256::ZERO },
     _ => if a < b { U256::ONE } else { U256::ZERO },
 }], jump: 1 };
-pub static SGT: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [match (a.is_neg(), b.is_neg()) {
+pub static SGT: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [match (a.is_neg(), b.is_neg()) {
     (true, false) => { U256::ZERO },
     (false, true) => { U256::ONE },
     _ => if a > b { U256::ONE } else { U256::ZERO },
 }], jump: 1 };
-pub static EQ: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [if a == b { U256::ONE } else { U256::ZERO }], jump: 1 };
-pub static ISZERO: TransitionFunction<1, 1> = |[a]: [u256; 1]| TransitionFunctionOutput { cost: 3, result: [if a == U256::ZERO { U256::ONE } else { U256::ZERO }], jump: 1 };
-pub static AND: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [a & b], jump: 1 };
-pub static OR: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [a | b], jump: 1 };
-pub static XOR: TransitionFunction<2, 1> = |[a, b]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [a ^ b], jump: 1 };
-pub static NOT: TransitionFunction<1, 1> = |[a]: [u256; 1]| TransitionFunctionOutput { cost: 3, result: [!a], jump: 1 };
-pub static BYTE: TransitionFunction<2, 1> = |[i, x]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [if i > 31 { U256::ZERO } else { (x >> (8 * (31 - i))) & 0xFF }], jump: 1 };
-pub static SHL: TransitionFunction<2, 1> = |[shift, value]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [match TryInto::<u8>::try_into(shift) {
+pub static EQ: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [if a == b { U256::ONE } else { U256::ZERO }], jump: 1 };
+pub static ISZERO: TransitionFunction<1, 1> = |[a]| TransitionFunctionOutput { cost: 3, result: [if a == U256::ZERO { U256::ONE } else { U256::ZERO }], jump: 1 };
+pub static AND: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [a & b], jump: 1 };
+pub static OR: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [a | b], jump: 1 };
+pub static XOR: TransitionFunction<2, 1> = |[a, b]| TransitionFunctionOutput { cost: 3, result: [a ^ b], jump: 1 };
+pub static NOT: TransitionFunction<1, 1> = |[a]| TransitionFunctionOutput { cost: 3, result: [!a], jump: 1 };
+pub static BYTE: TransitionFunction<2, 1> = |[i, x]| TransitionFunctionOutput { cost: 3, result: [if i > 31 { U256::ZERO } else { (x >> (8 * (31 - i))) & 0xFF }], jump: 1 };
+pub static SHL: TransitionFunction<2, 1> = |[shift, value]| TransitionFunctionOutput { cost: 3, result: [match TryInto::<u8>::try_into(shift) {
     Ok(shift) => value.wrapping_shl(shift.into()),
     _ => U256::ZERO,
 }], jump: 1 };
-pub static SHR: TransitionFunction<2, 1> = |[shift, value]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [match TryInto::<u8>::try_into(shift) {
+pub static SHR: TransitionFunction<2, 1> = |[shift, value]| TransitionFunctionOutput { cost: 3, result: [match TryInto::<u8>::try_into(shift) {
     Ok(shift) => value.wrapping_shr(shift.into()),
     _ => U256::ZERO,
 }], jump: 1 };
-pub static SAR: TransitionFunction<2, 1> =|[shift, value]: [u256; 2]| TransitionFunctionOutput { cost: 3, result: [match (TryInto::<u8>::try_into(shift), value.is_neg()) {
+pub static SAR: TransitionFunction<2, 1> =|[shift, value]| TransitionFunctionOutput { cost: 3, result: [match (TryInto::<u8>::try_into(shift), value.is_neg()) {
     (Ok(shift), false) => value.wrapping_shr(shift.into()),
     (Ok(shift), true) => { if shift == 0 { value } else { !(U256::ONE.wrapping_shl((255 - shift + 1).into()) - 1) | value.wrapping_shr(shift.into()) } },
     (Err(_), false) => U256::ZERO,
