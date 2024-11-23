0c8f7e0332a5ea78d8bbfe205e78cfd782f91c46
François Guérin <francoisguerin1@hotmail.fr>
2024-11-23 10:44:12 +0100

    Rename `transition_builder` to `execute_transition`
    This function has become more than a simple builder over time.

--- src/state.rs
+++ src/state.rs
@@ -34,7 +34,7 @@ impl State {
         }
     }
 
-    fn transition_builder<const I: usize, const O: usize>(&mut self, f: TransitionFunction<I, O>) -> Result<TransitionOutput, String> {
+    fn execute_transition<const I: usize, const O: usize>(&mut self, f: TransitionFunction<I, O>) -> Result<TransitionOutput, String> {
         let mut context = TransitionContext {
             code: &self.transaction.data,
             gas: &self.remaining_gas,
@@ -61,44 +61,44 @@ impl State {
         Ok(TransitionOutput { cost: output.cost, jump: output.jump })
     }
 
-    fn stop(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(STOP) }
-    fn add(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(ADD) }
-    fn mul(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(MUL) }
-    fn sub(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SUB) }
-    fn div(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(DIV) }
-    fn sdiv(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SDIV) }
-    fn r#mod(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(MOD) }
-    fn smod(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SMOD) }
-    fn addmod(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(ADDMOD) }
-    fn mulmod(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(MULMOD) }
-    fn exp(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(EXP) }
-    fn signextend(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SIGNEXTEND) }
-    fn lt(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(LT) }
-    fn gt(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(GT) }
-    fn slt(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SLT) }
-    fn sgt(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SGT) }
-    fn eq(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(EQ) }
-    fn iszero(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(ISZERO) }
-    fn and(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(AND) }
-    fn or(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(OR) }
-    fn xor(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(XOR) }
-    fn not(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(NOT) }
-    fn byte(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(BYTE) }
-    fn shl(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SHL) }
-    fn shr(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SHR) }
-    fn sar(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SAR) }
-    fn pop(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(POP) }
-    fn mload(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(MLOAD) }
-    fn mstore(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(MSTORE) }
-    fn mstore8(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(MSTORE8) }
-    fn sload(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SLOAD) }
-    fn sstore(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(SSTORE) }
-    fn jump(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(JUMP) }
-    fn jumpi(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(JUMPI) }
-    fn pc(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(PC) }
-    fn msize(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(MSIZE) }
-    fn gas(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(GAS) }
-    fn jumpdest(&mut self) -> Result<TransitionOutput, String> { self.transition_builder(JUMPDEST) }
+    fn stop(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(STOP) }
+    fn add(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(ADD) }
+    fn mul(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(MUL) }
+    fn sub(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SUB) }
+    fn div(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(DIV) }
+    fn sdiv(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SDIV) }
+    fn r#mod(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(MOD) }
+    fn smod(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SMOD) }
+    fn addmod(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(ADDMOD) }
+    fn mulmod(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(MULMOD) }
+    fn exp(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(EXP) }
+    fn signextend(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SIGNEXTEND) }
+    fn lt(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(LT) }
+    fn gt(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(GT) }
+    fn slt(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SLT) }
+    fn sgt(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SGT) }
+    fn eq(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(EQ) }
+    fn iszero(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(ISZERO) }
+    fn and(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(AND) }
+    fn or(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(OR) }
+    fn xor(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(XOR) }
+    fn not(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(NOT) }
+    fn byte(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(BYTE) }
+    fn shl(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SHL) }
+    fn shr(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SHR) }
+    fn sar(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SAR) }
+    fn pop(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(POP) }
+    fn mload(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(MLOAD) }
+    fn mstore(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(MSTORE) }
+    fn mstore8(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(MSTORE8) }
+    fn sload(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SLOAD) }
+    fn sstore(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(SSTORE) }
+    fn jump(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(JUMP) }
+    fn jumpi(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(JUMPI) }
+    fn pc(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(PC) }
+    fn msize(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(MSIZE) }
+    fn gas(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(GAS) }
+    fn jumpdest(&mut self) -> Result<TransitionOutput, String> { self.execute_transition(JUMPDEST) }
 }
 
 #[cfg(test)]
@@ -114,17 +114,17 @@ mod tests {
 
         assert_eq!(state.remaining_gas, 7);
 
-        assert_eq!(state.transition_builder(
+        assert_eq!(state.execute_transition(
             |_, _: [u256; 0]| Ok(TransitionFunctionOutput { cost: 3, result: [], jump: 1 })
         ), Ok(TransitionOutput { cost: 3, jump: 1 }));
         assert_eq!(state.remaining_gas, 4);
 
-        assert_eq!(state.transition_builder(
+        assert_eq!(state.execute_transition(
             |_, _: [u256; 0]| Ok(TransitionFunctionOutput { cost: 3, result: [], jump: 1 })
         ), Ok(TransitionOutput { cost: 3, jump: 1 }));
         assert_eq!(state.remaining_gas, 1);
 
-        assert_eq!(state.transition_builder(
+        assert_eq!(state.execute_transition(
             |_, _: [u256; 0]| Ok(TransitionFunctionOutput { cost: 3, result: [], jump: 1 })
         ), Err("Out of gas".to_string()));
         assert_eq!(state.remaining_gas, 0);
@@ -137,7 +137,7 @@ mod tests {
     fn transition_builder_fails_if_not_enough_parmeters_in_stack() {
         let mut state = State::new(StateParameters { initial_storage: Default::default(), transaction: Transaction { data: Default::default(), gas: 20 } });
 
-        assert_eq!(state.transition_builder(
+        assert_eq!(state.execute_transition(
             |_, input: [u256; 1]| Ok(TransitionFunctionOutput { cost: 3, result: [input[0]], jump: 1 })
         ), Err("Stack is empty".to_string()));
     }
@@ -146,7 +146,7 @@ mod tests {
     fn transition_builder_fails_if_too_much_outputs() {
         let mut state = State::new(StateParameters { initial_storage: Default::default(), transaction: Transaction { data: Default::default(), gas: 20 } });
 
-        assert_eq!(state.transition_builder(
+        assert_eq!(state.execute_transition(
             |_, _input: [u256; 0]| Ok(TransitionFunctionOutput { cost: 3, result: [U256::ZERO; 1025], jump: 1 })
         ), Err("Stack overflow".to_string()));
     }
@@ -155,7 +155,7 @@ mod tests {
     fn transition_builder_fails_if_transition_function_fails() {
         let mut state = State::new(StateParameters { initial_storage: Default::default(), transaction: Transaction { data: Default::default(), gas: 20 } });
 
-        assert_eq!(state.transition_builder(
+        assert_eq!(state.execute_transition(
             |_, _input: [u256; 0]| Result::<TransitionFunctionOutput<0>, String>::Err("Error message".to_string())
         ), Err("Error message".to_string()));
     }
